5687f71c9ad1fc8fcb932988389670db0cacbe02
Chouti <l0nl1f3@gmail.com>
2022-09-17 11:15:47 +0800

    update ignore

--- testcase/c/global_values.c
+++ testcase/c/global_values.c
@@ -1,6 +1,6 @@
 int GLOBAL_X = 20;
 int GLOBAL_Y = 31;
-int z_GLOBAL_Z = 10;
+int GLOBAL_Z = 10;
 
 int inc(int x)  // { return x; };
 {
@@ -9,5 +9,5 @@ int inc(int x)  // { return x; };
 
 int test(int x) {
   inc(x);
-  return GLOBAL_X + z_GLOBAL_Z == GLOBAL_Y;
+  return GLOBAL_X + GLOBAL_Z == GLOBAL_Y;
 }
