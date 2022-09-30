cb0742fdaee2d38788f21cfe2056611c1be55433
Chouti <l0nl1f3@gmail.com>
2022-09-30 10:56:11 +0800

    cleanup

--- dl-lib/src/lib.rs
+++ dl-lib/src/lib.rs
@@ -46,10 +46,7 @@ fn main() -> ! {
     init_heap();
     let p_start = unsafe { &_binary_module_bin_start as *const u8 };
     let p_end = unsafe { &_binary_module_bin_end as *const u8 };
-    dbg!(p_start);
-    dbg!(p_end);
     let module = module::dl_load(p_start, p_end, None);
-    dbg!(&module);
     let entry = dl_entry_by_name(&module, "test");
     let f = unsafe { mem::transmute::<usize, fn(u32) -> u32>(entry) };
     dbg!(call_func_arg(f, 1));
