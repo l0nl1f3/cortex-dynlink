d013a7547b6d7ff3db2ff8b759bfd9ad6f03e8e7
Chouti <l0nl1f3@gmail.com>
2022-09-30 10:54:42 +0800

    fix alignment issue

--- dl-lib/run.sh
+++ dl-lib/run.sh
@@ -1,4 +1,4 @@
 cargo build
-llvm-objcopy --input-target binary --output-target elf32-littlearm --rename-section .data=.rodata --binary-architecture arm $1.bin $1.o
+arm-none-eabi-objcopy --input binary --output elf32-littlearm --set-section-alignment .data=4 --rename-section .data=.text --binary-architecture arm $1.bin $1.o
 ld.lld -Tlink.ld target/thumbv7em-none-eabi/debug/libdl_lib.a $1.o -o executable
 /home/zhouyi/.local/xPacks/@xpack-dev-tools/qemu-arm/7.0.0-1.1/.content/bin/qemu-system-gnuarmeclipse --board STM32F4-Discovery -nographic -semihosting-config enable=on,target=native -S -gdb tcp::3333 -kernel executable
